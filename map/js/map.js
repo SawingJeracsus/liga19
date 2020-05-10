9495f62190d1c24e202a36fd56434974b6a69c74
SawingJeracsus <64886497+SawingJeracsus@users.noreply.github.com>
2020-05-10 19:40:29 +0300

    Мапа - дороблена (уху)

--- map/js/map.js
+++ map/js/map.js
@@ -1,7 +1,56 @@
 class Map {
-  constructor(points = [], wrapper) {
+  constructor(points = [], wrapper, map, config) {
     this.points = points;
     this.wrapper = wrapper;
+    this.addEl = document.getElementById(config.add);
+    this.delet = document.getElementById(config.delet);
+
+    this.addEl.addEventListener('click', ()=>{
+      let text = prompt('Введіть назву:');
+      this.addNew(text);
+    })
+
+    this.delet.addEventListener('click', ()=> {
+      let text = prompt('Введіть назву для виталення:');
+      if(confirm('Ви впевнені?')){
+        console.log(this);
+        this.delete(text)
+      }
+    })
+    this.mapImg = document.getElementById(map);
+    this.mapWidth = this.mapImg.offsetWidth;
+    this.mapHeight = this.mapImg.offsetHeight;
+    for(let pointId in this.points){
+      let point = this.points[pointId];
+      point.setWidthHeight(this.mapWidth, this.mapHeight)
+    }
+  }
+  addNew(text){
+    let wrapper = document.getElementsByClassName(this.wrapper)[0];
+
+    wrapper.innerHTML = wrapper.innerHTML + `<button class="point" id="${this.points.length+1}">${text}</button>`;
+    let id = this.points.push(new Point(this.points.length+1, this.points.length+1)) - 1;
+    this.points[id].setWidthHeight(this.mapWidth, this.mapHeight);
+
+    this.points[id].push({
+      x: 20,
+      y: 20,
+    });
+  }
+  delete(text){
+    let success = false;
+    for(let pointId in this.points){
+      let point = this.points[pointId];
+      if(point.text === text){
+        // console.log(point);
+        point.el.remove();
+        this.points[pointId] = null;
+        success = true;
+      }
+    }
+    if(!success){
+      alert("Такого ще немає, щоб його ... ЗНИЩУВАТИ!!!");
+    }
   }
   up(e){
     for(let pointId in this.points){
@@ -38,7 +87,11 @@ class Map {
     let saveArray = [];
     for(let pointId in this.points){
       let point = this.points[pointId];
-      saveArray.push(point.save());
+      try {
+        saveArray.push(point.save());
+      } catch (e) {
+        console.log('Пусто...');
+      }
     }
     return JSON.stringify(saveArray);
   }
@@ -53,6 +106,7 @@ class Map {
       let point = this.points[pointId];
       point.destroy();
     }
+    // console.log(this.points);
     // console.log(arrayRestoring);
     let wrapper = document.getElementsByClassName(this.wrapper)[0];
     this.points = [];
@@ -61,6 +115,7 @@ class Map {
       let point = arrayRestoring[pointID];
       wrapper.innerHTML = wrapper.innerHTML + `<button class="point" id="${point.id}">${point.text}</button>`;
       let id = this.points.push(new Point(point.id, point.id)) - 1;
+      this.points[id].setWidthHeight(this.mapWidth, this.mapHeight);
       this.points[id].restore(point);
     }
 
@@ -82,6 +137,10 @@ class Point {
   rebindEL(){
       this.el = document.getElementById(this.elID);
   }
+  setWidthHeight(w,h){
+    this.width = w;
+    this.height = h;
+  }
   up(e){
     this.moving = true;
   }
@@ -91,8 +150,12 @@ class Point {
     let x = e.clientX - this.el.offsetWidth/2;
     let y = e.clientY - this.el.offsetHeight/2;
 
-    this.x = x / window.innerWidth;
-    this.y = y / window.innerHeight;
+
+    x  =   e.clientX - this.el.offsetWidth/2 - (window.innerWidth-this.width)/2;
+    y  =   e.clientY - this.el.offsetHeight/2 - (window.innerHeight-this.height)/2;
+
+    this.x = x / this.width ;
+    this.y = y / this.height;
 
     this.el.setAttribute("style", 'top: '+y+'px; left: '+x+'px;');
   }
@@ -100,13 +163,14 @@ class Point {
   windowchangeHandler(){
     this.rebindEL();
 
-    let x = this.x * window.innerWidth;
-    let y = this.y * window.innerHeight;
+    let x = this.x * this.width ;//+ (window.innerWidth-this.width)/2;
+    let y = this.y * this.height;// + (window.innerHeight-this.height)/2;
 
     this.el.setAttribute("style", 'top: '+y+'px; left: '+x+'px;');
   }
   down(e){
     this.moving = false;
+    this.windowchangeHandler()
   }
 
   save(){
@@ -127,11 +191,10 @@ class Point {
     this.el.remove();
   }
 }
-var map = new Map([
-  new Point('lubeshiv', 1),
-  new Point('buchin', 2),
-  new Point('zarika', 3)
-], 'map_wrapper')
+var map = new Map([], 'img', 'map',{
+  add: 'add',
+  delet: "delet"
+})
 
 var mousedown = false;
 document.addEventListener('mousedown', function (e) {
@@ -157,4 +220,4 @@ window.addEventListener('resize', () => {
   map.windowchangeHandler()
 });
 
-map.restore('[{"x":0.21693907875185736,"y":0.3496,"text":"Любешів","id":1},{"x":0.3514115898959881,"y":0.7944,"text":"Бучин","id":2},{"x":0.40936106983655274,"y":0.1832,"text":"Заріка","id":3}]')
+map.restore('[{"x":0.72890625,"y":0.03553921568627451,"text":"Любешів","id":1},{"x":0.40234375,"y":0.8909313725490197,"text":"Бучин","id":2},{"x":0.4046875,"y":0.0428921568627451,"text":"Заріка","id":3},{"x":0.19140625,"y":0.26838235294117646,"text":"Язівно","id":4}]')
